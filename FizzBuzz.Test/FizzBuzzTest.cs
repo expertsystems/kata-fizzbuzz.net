899d1b7b06bfa03bb676da1ef3d1f244a85c08ba
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2015-12-09 11:30:43 +0100

    Visual Basic FizzBuzz implemented

--- FizzBuzz.Test/FizzBuzzTest.cs
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -10,13 +10,106 @@ namespace FizzBuzz
         private static ILog LOG = LogManager.GetLogger(typeof(FizzBuzzTest));
 
         [Test]
-        public void FirstTest()
+        public void TwoReturnsTwo()
+        {
+            LOG.Info("Arrange");
+            
+
+            LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(2);
+
+            LOG.Info("Assert");
+            Assert.AreEqual("2", result);
+        }
+
+        [Test]
+        public void OneReturnsOne()
         {
             LOG.Info("Arrange");
 
+
             LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(1);
 
             LOG.Info("Assert");
+            Assert.AreEqual("1", result);
         }
+
+        [Test]
+        public void ThreeReturnsFizz()
+        {
+            LOG.Info("Arrange");
+
+
+            LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(3);
+
+            LOG.Info("Assert");
+            Assert.AreEqual("Fizz", result);
+        }
+
+
+        [Test]
+        public void NineReturnsFizz()
+        {
+            LOG.Info("Arrange");
+
+
+            LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(9);
+
+            LOG.Info("Assert");
+            Assert.AreEqual("Fizz", result);
+        }
+
+
+        [Test]
+        public void TenReturnsBuzz()
+        {
+            LOG.Info("Arrange");
+
+
+            LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(10);
+
+            LOG.Info("Assert");
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [Test]
+        public void ThirtyReturnsFizzBuzz()
+        {
+            LOG.Info("Arrange");
+
+
+            LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(30);
+
+            LOG.Info("Assert");
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+
+        [Test]
+        public void ThirteenReturnsFizz()
+        {
+            LOG.Info("Arrange");
+
+
+            LOG.Info("Act");
+            FizzBuzz fb = new FizzBuzz();
+            String result = fb.fizzbuzz(13);
+
+            LOG.Info("Assert");
+            Assert.AreEqual("Fizz", result);
+        }
+    
     }
+
 }
--- FizzBuzz.VB/FizzBuzz.vb
+++ FizzBuzz.VB/FizzBuzz.vb
@@ -1,11 +1,38 @@
 ﻿Imports Common.Logging
 
-Module FizzBuzz
-
-    Sub Main()
-
+Namespace FizzBuzz
+    Public Class FizzBuzz
 
 
-    End Sub
+        Function fizzbuzz(input As Integer) As String
 
-End Module
+            If isBuzz(input) Then
+                If isFizz(input) Then
+                    Return "FizzBuzz"
+                Else
+                    Return "Buzz"
+                End If
+            ElseIf isFizz(input) Then
+                Return "Fizz"
+            Else
+                Return input
+            End If
+
+        End Function
+
+        Function isFizz(input As Integer) As Boolean
+
+            Return (input Mod 3 = 0) OrElse CStr(input).Contains(3)
+
+        End Function
+
+        Function isBuzz(input As Integer) As Boolean
+
+            Return (input Mod 5 = 0)
+
+        End Function
+
+    End Class
+
+
+End Namespace
